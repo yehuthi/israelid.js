f4d1b3db17a5dfc98fc7a8e0f45695c129b5dc4e
yehuthi <yehuthith@gmail.com>
2024-10-05 21:15:04 +0300

    remove DigitalChar
    Inline it instead to minimize API surface area.

--- index.ts
+++ index.ts
@@ -11,13 +11,10 @@ const checksum = (id: string): number => {
 /** Validates the given ID. */
 export const valid = (id: string): boolean => checksum(id) % 10 === 0
 
-/** A digit character. */
-export type DigitChar = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
-
 /** Calcualtes the checksum digit for the given ID. */
-export const checksum_digit = (id: string): DigitChar => {
+export const checksum_digit = (id: string): '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
     const rem = checksum(id) % 10;
-    return rem === 0 ? '0' : String.fromCharCode(58 - rem) as unknown as DigitChar;
+    return rem === 0 ? '0' : String.fromCharCode(58 - rem) as ReturnType<typeof checksum_digit>;
 }
 
 export const control_complement = (id: string): 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => {
