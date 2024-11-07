9d99c727a906c6680993714c75097d2054df0e16
yehuthi <yehuthith@gmail.com>
2024-11-07 08:35:57 +0200

    format

--- index.ts
+++ index.ts
@@ -12,13 +12,19 @@ const checksum = (id: string): number => {
 export const valid = (id: string): boolean => checksum(id) % 10 === 0
 
 /** Calcualtes the checksum digit for the given ID. */
-export const checksum_digit = (id: string): '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+export const checksum_digit = (
+	id: string
+): '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
     const rem = checksum(id) % 10;
-    return rem === 0 ? '0' : String.fromCharCode(58 - rem) as ReturnType<typeof checksum_digit>;
+    return rem === 0 ?
+		'0' :
+		String.fromCharCode(58 - rem) as ReturnType<typeof checksum_digit>;
 }
 
 /** Computes a conotrol digit that would make the ID valid. */
-export const control_complement = (id: string): 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 => {
+export function control_complement(
+	id: string
+): 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 {
 	const rem = checksum(id) % 10;
 	if (rem == 0) return 0;
 	const is_doubled_position = id.length % 2;
@@ -26,7 +32,8 @@ export const control_complement = (id: string): 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 
 	const delta_is_even = delta % 2 == 0;
 
 	let result = delta;
-	if (!delta_is_even && is_doubled_position) result += 9; // this gets us a two-digit number that sums up to the delta
+	if (!delta_is_even && is_doubled_position) result += 9;
+	// ^ this gets us a two-digit number that sums up to the delta
 	if (is_doubled_position) result /= 2;
 	return result as ReturnType<typeof control_complement>;
 }
